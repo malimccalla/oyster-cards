88bdb39832389fb60d9af38e86c8909f778bf564
Mali Michael <malimccalla@gmail.com>
2016-07-11 20:55:07 +0100

    Implement touch in/out methods and check if card is in a journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -11,6 +11,22 @@ class Oystercard
     @balance += amount
   end
 
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
   private
 
   def maxium_limit_error
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -19,9 +19,51 @@ describe Oystercard do
     end
 
     it 'raises an error if balance exceeds maximum limit' do
-      expect{subject.top_up(91)}.to raise_error "Unable to top up, £#{Oystercard::MAXIMUM_LIMIT} is the maximum card limit"
+      maximum_limit = Oystercard::MAXIMUM_LIMIT
+      expect{subject.top_up(maximum_limit+1)}.to raise_error "Unable to top up, £#{maximum_limit} is the maximum card limit"
     end
 
+  end
+
+  describe '#deduct' do
+
+    it {is_expected.to respond_to(:deduct).with(1).argument}
+
+    it 'deducts an amount from the balance' do
+      subject.top_up(10)
+      expect{subject.deduct(5)}.to change{subject.balance}.by(-5)
+    end
+
+  end
+
+  describe '#in_journey?' do
+
+    it 'reports if a card is in journey' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it 'reports if a card is not in a journey' do
+      subject.touch_out
+      expect(subject).to_not be_in_journey
+    end
+
+  end
+
+  describe '#touch_in' do
+
+    it {is_expected.to respond_to(:touch_in)}
+
+    it 'allows a card to be touched in' do
+      expect(subject.touch_in).to eq(true)
+    end
+
+  end
+
+  describe '#touch_out' do
+    it 'allows a card to be touched out' do
+      expect(subject.touch_out).to eq(false)
+    end
   end
 
 end
