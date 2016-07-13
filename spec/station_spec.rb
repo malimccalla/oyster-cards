3d70d9c23fce535444bdbdca8de7f5efb49ed8b8
Mali Michael <malimccalla@gmail.com>
2016-07-13 16:14:16 +0100

    Create Station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,15 @@
+require 'station'
+
+describe Station do
+
+  subject {described_class.new("Angel",2)}
+
+  it 'knows its name' do
+    expect(subject.name).to eq("Angel")
+  end
+
+  it 'knows its zone' do
+    expect(subject.zone).to eq(2)
+  end
+
+end
